55e7b92f258f585a03134d253136b3db4c3b150b
Souvikdas10 <souvikd179@gmail.com>
2023-07-29 13:33:24 +0530

    Logout both Storage

--- src/app/Service/storage.service.ts
+++ src/app/Service/storage.service.ts
@@ -6,26 +6,41 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
-  setData(name: string, email: string, id: string, token: string) {
-    window.localStorage.setItem('name', name);
-    window.localStorage.setItem('email', email);
-    window.localStorage.setItem('id', id);
-    window.sessionStorage.setItem('token', token);
+  setData(name:string,email:string,id:string,token:string){
+    window.localStorage.setItem('name',name);
+    window.localStorage.setItem('email',email);
+    window.localStorage.setItem('id',id);
+    window.sessionStorage.setItem('token',token);
   }
-  getData() {
-    const saveData = [];
-    let data = {
-      name: window.localStorage.getItem('name'),
-      email: window.localStorage.getItem('email'),
-      id: window.localStorage.getItem('id'),
+  getData(){
+    const saveData=[];
+    let data={
+      name:window.localStorage.getItem('name'),
+      email:window.localStorage.getItem('email'),
+     id:window.localStorage.getItem('id'),
     }
     saveData.push(data);
     return saveData;
   }
-  getToken() {
+  // token collect
+  getToken(){
     return window.sessionStorage.getItem('token')
   }
+// logout using SessionStorage
+
+  // getDestroy(){
+  //   return window.sessionStorage.clear()
+  // }
+  
+// Logout using Both Storage
+
   getDestroy() {
-    return window.sessionStorage.clear()
+    const destroyData=[];
+    let data={
+      localStorage:window.localStorage.clear(),
+      sessionStorage :window.sessionStorage.clear()
+    }
+    destroyData.push(data)
+    return destroyData;
   }
 }
